fc680feed5ec2c6ab5c922b46ac4adab6bc8f3aa
Thilina <imwageesgha@gmail.com>
2020-10-04 18:36:32 +0530

    init commit

---
+++ moragpacalculator-server/src/main/java/com/myapps/moragpacalculatorserver/MoragpacalculatorServerApplication.java
@@ -0,0 +1,13 @@
+package com.myapps.moragpacalculatorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoragpacalculatorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoragpacalculatorServerApplication.class, args);
+	}
+
+}
---
+++ moragpacalculator-server/src/test/java/com/myapps/moragpacalculatorserver/MoragpacalculatorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapps.moragpacalculatorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoragpacalculatorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
